4314af9526685952fbc8db13d588fd739a5e77af
Ana Bergese <violetagele22@gmail.com>
2023-08-26 17:30:01 +0200

    deleted file gptprocessed and added it into the gptProcessing file. Tested and working.

--- gptProcessing.js
+++ gptProcessing.js
@@ -1,10 +1,45 @@
-import { gptProcess } from './gptProcess.js';
+import fetch from 'node-fetch';
+const API_TOKEN = 'Bearer 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';
+const TRANSCRIPT_API_URL = 'https://api.openai.com/v1/completions';
 
 export async function processTranscription(transcriptionResult) {
   // Process the transcriptionResult using the GPT API
-  const processedResult = await gptProcess(transcriptionResult);
+  const headers = {
+    Authorization: API_TOKEN,
+    "Content-Type": "application/json"
+  };
 
-  // You can add more processing steps here if needed
+  const requestBody = {
+    "model": "text-davinci-003",
+    "prompt": `Your answer can have a minimum length: 50 characters and maximum length: 300 characters. You are an artificial assistant for people that is alone, please, answer to this: ${transcriptionResult}`,
+    "suffix": "\n// Finish",
+    "temperature": 0.5,
+    "max_tokens": 256,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0,
+    "stop": [
+      "\n// Finish"
+    ]
+  };
+
+// Call the GPT API to process the input text
+  const response = await fetch(TRANSCRIPT_API_URL, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(requestBody),
+  });
+
+  if (!response.ok) {
+    throw new Error(`GPT request failed with status ${response.status}`);
+  }
+
+  // Retrieve the ID of the transcript from the response data
+  const responseGPT = await response.json();
+    // Extract and return the generated text from the response
+  const processedResult = responseGPT.choices[0].text.trim();
+
+
 
   return processedResult;
 }
