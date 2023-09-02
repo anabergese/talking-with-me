df3a4abcf8dfa4cfa819721deff31718acf83175
Ana Bergese <violetagele22@gmail.com>
2023-09-02 15:47:31 +0200

    preload gifImage from my project by creating an Image object and setting its src attribute to the relative path of my  image

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,7 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.getElementById('startRecording');
     const stopButton = document.getElementById('stopRecording');
     const mic_icon = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" width="80" height="80" x="0" y="0" viewBox="0 0 64 64" style="enable-background:new 0 0 512 512" xml:space="preserve" class=""><g><path d="M32 4a28 28 0 1 0 28 28A28.005 28.005 0 0 0 32 4zm-6.96 17.46a6.96 6.96 0 0 1 13.92 0v8.15a6.96 6.96 0 0 1-13.92 0zm18.41 8.38a11.522 11.522 0 0 1-10.19 11.37v5.77h4.65v2.52H26.09v-2.52h4.65v-5.77a11.522 11.522 0 0 1-10.19-11.37v-3.67h2.52v3.67a8.93 8.93 0 0 0 17.86 0v-3.67h2.52z" fill="#00a884" data-original="#000000" opacity="1"></path></g></svg>`;
+    const gifURL = "/images/audiorecording.gif"
     
     // Hide the SVG
     const svg = startButton.querySelector("svg");
@@ -9,11 +10,15 @@ document.addEventListener('DOMContentLoaded', function () {
     let recordedChunks = [];
     let audioUrl;
 
+
+    // Preload the GIF image
+    const gifImage = new Image();
+    gifImage.src = gifURL;
+
     startButton.addEventListener('click', async () => {
       recordedChunks = [];
       svg.style.display = "none";
        // Set the GIF as inner HTML
-      const gifURL = "https://i.gifer.com/origin/17/17b79d4cc4204635eaa22c3a66112c1a_w200.gif";
       startButton.innerHTML = `<img src=${gifURL} id="startButton_gif" alt="Recording GIF" width="80" height="80">`;
 
       try {
