9fa5d33e83ff01d0c128a05415e1a83ec096b295
Ana Bergese <violetagele22@gmail.com>
2023-09-13 13:14:48 +0200

    undommented dist and netlify from gitignore

---
+++ .netlify/functions-serve/curriculum/src/functions/curriculum.js
@@ -0,0 +1,60 @@
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// functions/curriculum.js
+var curriculum_exports = {};
+__export(curriculum_exports, {
+  curriculum: () => curriculum
+});
+module.exports = __toCommonJS(curriculum_exports);
+var curriculum = `
+Curriculum: Ana Bele\u0301n Bergese
+I am from Argentina, and I live in Barcelona since 5 years.
+
+FULLSTACK WEB DEVELOPER						
+I consider myself a motivated-happy person with a desire to learn and grow professionally. I am a very friendly person and I am looking for a fast-paced work environment. I am excited to be part of your team and contribute with my technical skills, soft skills, and my willingness to learn and develop.
+Over the past 3 years, I have dedicated myself wholeheartedly to the study and development of software. I discovered a subject that truly impassions me, and I consider myself very fortunate for it.
+I take pleasure in working independently, tackling complex problems on my own, and have created several apps autonomously. Through this, I have honed my own work methodology and problem-solving approach. However, this doesn't mean I don't enjoy working in a team; quite the opposite, I highly value it and believe that effective intra-team communication and collaboration with other teams are crucial.
+
+Last Experiences	
+React Project: Adapted the design of the TaskMate App, utilizing front-end programming skills, to cater specifically to individuals with Autism Spectrum Disorder (ASD) or Attention Deficit Disorder (ADD/ADHD). I applied: Typescript, TDD, Jest, Unit Tests, Hooks, HTML, CSS, JS, Rest APIs.		
+Technical Support Engineer: Landbot | 2021 - Present (Barcelona). Assisting with issues related to HTML, CSS, and Javascript, as well as integrations with other platforms and APIs, such as Dialogflow, Integromat, Google Map APIs, Zapier, Make, and Airtable. I also played a key role in creating bot flows using Chat GPT and supporting prompt engineering.
+Technical Support Analyst: Nice People At Work (NPAW) | 2020 (Barcelona). Daily control of the operations and functions of YOUBORA, a video analytics tool. Detection and management of bugs, using troubleshooting steps and Google Chrome developers tools. Communicator between teams in an Agile Environment: clients, product, front-end, back-end, plugins, and customer success.
+Advertiser Support at Facebook: Teleperformance Company | 2019 (Lisbon). Support and guidance to small-to-medium sized businesses through the issues generated with Facebook Ads Account: delivery, catalogs, pixels, Shopify, bugs, promo codes, etc. Use of chat, phone, and email.
+						
+Education:					
+Fullstack Web Developer Bootcamp: Le Wagon | 2020 (Barcelona). 
+Degree in Social Anthropology: National University of Co\u0301rdoba | 2012-2017 (Argentina)
+UX and UI expert course: National Technological University | 2020 (online) 
+						
+Technical skills:				
+- NodeJS, ExpressJS -HTML, CSS, JavaScript - React  - Typescript - TDD, Unit Tests, Jest  - GPT  - Bootstrap - SQL - Git, GitHub - REST APIs - Stimulus - Heroku and Ruby on Rails. - Slack and Jira, Confluence - Figma - Bot Maker
+						
+Soft skills	:				
+- Team commitment  - Adaptability  - Empathy  - Investigation and Resolution skills - Fast learning  - High volume concept learning - Time management
+						
+Languages						
+- English, advanced- Spanish, mother tongue - Portuguese, beginner
+
+It is mandatory to answer with a maximum of 200 characters, it can be less but not more.
+`;
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  curriculum
+});
+//# sourceMappingURL=curriculum.js.map
