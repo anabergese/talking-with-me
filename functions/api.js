655e86f8a8703dd429c45fefcf332261f5fca770
Ana Bergese <violetagele22@gmail.com>
2023-10-02 19:54:07 +0200

    modified line 12

--- functions/api.js
+++ functions/api.js
@@ -9,7 +9,7 @@ const app = express();
 const router = Router();
 
 let path = require('path');
-app.set("views", path.join(__dirname, "..", "views"));
+app.set("views", path.join(__dirname, "views"));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
