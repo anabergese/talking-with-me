3310bddb53c86d5575aeb225e28fa280318dc21f
Ana Belén Bergese <anabelenbergese@MacBook-Pro.local>
2025-07-29 12:58:36 +0200

    Build: added n8n integration

--- src/controllers/audioController.js
+++ src/controllers/audioController.js
@@ -1,6 +1,7 @@
 import { convertSpeechToText } from '../services/speechToText.js';
 import { processTranscription } from '../services/gptProcessing.js';
 import { convertTextToSpeech } from '../services/textToSpeech.js';
+import axios from 'axios'; 
 
 export const handleAudioUpload = async (req, res) => {
   if (!req.file) {
@@ -24,6 +25,14 @@ export const handleAudioUpload = async (req, res) => {
       transcriptionresult: transcriptionResult,
       audiocontent: audioTag,
     });
+
+    // Send data to n8n workflow
+    const webhookUrl = 'https://anabergese.app.n8n.cloud/webhook/8b02bb56-8931-4e71-9d3a-cf388bea42a4';
+    await axios.post(webhookUrl, {
+      user_query: transcriptionResult.text,
+      ai_answer: processedResult,
+    });
+
   } catch (error) {
     console.error('Error processing audio:', error);
     res.status(500).json({ message: 'Error processing audio' });
